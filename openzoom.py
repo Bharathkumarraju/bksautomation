26c3d1f0735bee5ebff7e53c986d5cc1a014bea4
Bharath <bharath@hooq.tv>
2019-11-15 17:22:52 +0800

    Test commit

---
+++ openzoom.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://zoom.us/j/3917185663")
---
+++ test.py
@@ -0,0 +1,7 @@
+import webbrowser as ac
+chrome = ac.get('Chrome')
+chrome.open('https://zoom.us/j/3917185663')
+
+
+
+
