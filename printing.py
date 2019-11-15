e349e832b6261a2a8ef2fa90f52bf74b5ed1bbcf
Bharath <bharath@hooq.tv>
2019-11-15 17:47:57 +0800

    Test the code

--- printing.py
+++ printing.py
@@ -4,3 +4,7 @@ print("my name is {my_name}")
 
 print(f"my name is {my_name}") # f fucks here ;) sorry formats here
 
+how_many_types_of_people = 1000
+
+types_of_people = f"There are {how_many_types_of_people} type of people in the world"
+print(types_of_people)
