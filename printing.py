aa308eff2f6a05f1cd09bf041cb036fee5950d85
Bharath <bharath@hooq.tv>
2019-11-15 18:08:38 +0800

    Test the print string

--- printing.py
+++ printing.py
@@ -8,3 +8,37 @@ how_many_types_of_people = 1000
 
 types_of_people = f"There are {how_many_types_of_people} type of people in the world"
 print(types_of_people)
+
+print("")
+
+binary = "binary"
+do_not = "don't"
+
+y = f" those who know {binary} and who {do_not}"
+x = f"there are {how_many_types_of_people} types of people in the world"
+
+print(x + y)
+
+hilarious = False
+
+joke_evaluation = "Isn't the joke is funny?! "
+
+joke_evaluation_1 = "Isn't the joke is funny?! {}"
+# how to use format ....
+
+print(joke_evaluation.format(hilarious))
+print(joke_evaluation_1.format(hilarious))
+
+print("")
+w = "This is the left side of ..."
+e = "a string with a right side"
+
+print(w + e)
+
+
+
+
+
+
+
+
